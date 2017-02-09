b4343f830e26e7a625a5b4cbe2316d9df00e7419
amitportnoy <amitportnoy@dalet.com>
2017-02-09 10:45:46 +0200

    add support for npm scopes

--- npm-version-provider.js
+++ npm-version-provider.js
@@ -1,6 +1,6 @@
 const request = require('request-promise-native');
 
 module.exports = async function npmVersionProvider({name, tag}) {
-  const packageInfo = await request(`https://registry.npmjs.org/${name}`, {json: true});
+  const packageInfo = await request(`https://registry.npmjs.org/${name.replace('/', '%2F')}`, {json: true});
   return packageInfo.versions[packageInfo['dist-tags'][tag]].version;
 };
